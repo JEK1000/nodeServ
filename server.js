6a31f8c4f29cded3fbc092727a5466f4bd3e8c5c
Jason <jasonekildare@gmail.com>
2023-05-03 23:57:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({
 
 const connection = mysql.createConnection({
   host: '173.254.104.55',
-  user: 'jkildare_jason',
+  user: 'jkildare_user',
   password: 'Jaden0102!',
   database: 'jkildare_mydb'
 });
