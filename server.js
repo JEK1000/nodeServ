c9a8833a7e5acde11ba512e42bba772f53baf595
Jason <jasonekildare@gmail.com>
2023-05-04 13:47:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get("/test", (req,res)=>{
   res.json("Hello from backend!");
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = 80;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
