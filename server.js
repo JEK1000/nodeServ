5694e77b41601d4719870411bdfcad6f29df7bf5
Jason <jasonekildare@gmail.com>
2023-05-10 15:42:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: 'https://jkildare.com',
+  origin: 'https://student.jkildare.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
