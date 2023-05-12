3383cfa822619d54fc5bca4376d89865dd66971d
Jason <jasonekildare@gmail.com>
2023-05-12 13:55:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const app = express();
 app.use(cookieParser());
