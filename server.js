e70b948402529ae0f97160b6d423d0c0c6406d22
Jason <jasonekildare@gmail.com>
2023-06-07 12:30:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
+const { createServer } = require('@railway/app');
+
+// Enable CORS
+app.use(createServer().cors());
 
 app.get("/test", (req,res)=>{
   res.json("Hello from backend!");
