a15c2dc243fd2e4615d4780b738150c08d65532a
Jason <jasonekildare@gmail.com>
2023-06-07 12:49:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/test", (req,res)=>{
   res.json("Hello from backend!");
