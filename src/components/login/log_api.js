26094c27858af437da9bc3fc4dc660a2da68f87c
Jason <jasonekildare@gmail.com>
2023-06-07 07:59:58 -0400

    Update log_api.js

--- src/components/login/log_api.js
+++ src/components/login/log_api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const login = async (FormData) => {
   try {
-    const response = await axios.post('http://nodeserv-production.up.railway.app/stud', { 
+    const response = await axios.post('https://nodeserv-production.up.railway.app/stud', { 
       FormData
     },{ withCredentials: true });
     return response.data;
