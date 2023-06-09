9c7b64c2f2b5827e30cf83aa1890e2a4e9cc5c65
Jason <jasonekildare@gmail.com>
2023-06-09 01:53:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://studentregistration-production.up.railway.app'
+  origin: 'https://studentregistration-production.up.railway.app',
+  credentials: true // Allow credentials (cookies)
 }));
 
 const mysql = require('mysql2');
