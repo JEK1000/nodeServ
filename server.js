dfba81f0ef270a4b1b1ae17262a1f55ec1991532
Jason <jasonekildare@gmail.com>
2023-07-05 18:38:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://jkildare.com/student',
+  origin: 'https://jkildare.com',
   credentials: true // Allow credentials (cookies)
 }));
 
