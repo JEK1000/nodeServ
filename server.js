69c5efa3006689112ec2a04aa22738ec09335172
Jason <jasonekildare@gmail.com>
2023-07-06 16:21:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.post("/stud", (req, res) => {
     } else {
       if (results.length > 0){
         const userId = results[0].student_ID;
-        res.cookie('user_id', JSON.stringify(userId), { httpOnly: false, domain: '.up.railway.app', sameSite: 'None', secure: true });
+        res.cookie('user_id', JSON.stringify(userId), { httpOnly: false, domain: '.up.railway.app' });
         console.log("user_id cookie server side: ", userId);
         res.send(true);
       }
